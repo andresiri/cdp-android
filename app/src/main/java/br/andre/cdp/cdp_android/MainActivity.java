51f6644515193d53acc5caeec6374be0dfff9b7e
Andre Miranda <andremirannda@gmail.com>
2017-05-09 20:22:16 -0300

    Adding butter knife.

--- app/src/main/java/br/andre/cdp/cdp_android/MainActivity.java
+++ app/src/main/java/br/andre/cdp/cdp_android/MainActivity.java
@@ -2,12 +2,22 @@ package br.andre.cdp.cdp_android;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.tv_hello_world) TextView tv_hello_word;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        ButterKnife.bind(this);
+
+        tv_hello_word.setText(R.string.xico);
+
         setContentView(R.layout.activity_main);
     }
 }
